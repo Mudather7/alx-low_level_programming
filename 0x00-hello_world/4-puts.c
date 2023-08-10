4ea416ed5dae7e6487c528a476baec47ac4c17a2
Mudather7 <mudatherabdo00@gmail.com>
2023-08-10 09:20:56 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
 /**
- * hello c
+ * main Entry point
+ * return : always 0
+ *
  */
 int main(void)
 {
+
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
