e11ecbf61e52521f47174bb3131ab7a96462a96d
Mudather7 <mudatherabdo00@gmail.com>
2023-08-10 11:20:31 -0700

    alx-low_level_programming

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
-/*
- * main -it print out output by puts function
+/**
+ *main -A program that print output by puts function
  *
- * return : Always (0)
+ *return: 0 (seccess)
  *
  */
-int main(void);
+int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle\n");
-	return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
