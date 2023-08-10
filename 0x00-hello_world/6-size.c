0b02eedb92adf3888aed75d0223cf4db1f2803af
Mudather7 <mudatherabdo00@gmail.com>
2023-08-10 13:34:02 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -13,6 +13,6 @@ int main(void)
 	printf("size ot a int : %d byte(s)\n", sizeof(int));
 	printf("size of a long int : %d byte(s)\n", sizeof(long int));
 	printf("size of a long long int : %d byte(s)\n", sizeof(long long int));
-	printf("size of a fload : %ld byte(s)\n", sizof(float));
+	printf("size of a fload : %d byte(s)\n", sizof(float));
 	return (0);
 }
