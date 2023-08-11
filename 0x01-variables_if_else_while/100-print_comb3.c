0e9399a85080f087ffc7d755d29ae97ba846228a
Mudather7 <mudatherabdo00@gmail.com>
2023-08-11 13:22:44 -0700

    100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i = 0;
+	int j = 0;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			if (i != j && i < j)
+			{
+				putchar(i);
+				putchar(j);
+			}
+			if (i + j != 17)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+
+		}
+		j++;
+	}
+	i++;
+	putchar('\n');
+	return (0);
+}
+
