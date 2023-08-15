329edb72e60a26553b7d301fef1f504ed8cc7f8a
Mudather7 <mudatherabdo00@gmail.com>
2023-08-15 14:27:48 -0700

    103-fibonacci.c

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	unsigned long n1 = 0, n2 = 1, sum;
+	float total_sum;
+
+	while (n2)
+	{
+		if (sum > 4000000)
+			break;
+		if ((sum % 2) == 0)
+			total_sum += sum;
+		n1 = n2;
+		n2 = sum;
+	}
+	printf("%.0f\n", total_sum);
+
+	return (0);
+}
