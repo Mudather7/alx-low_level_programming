fad89615c94e674e1db3efb02a3aa7c52b53dcb8
Mudather7 <mudatherabdo00@gmail.com>
2023-08-15 11:27:01 -0700

    11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -12,17 +12,17 @@ void print_to_98(int i)
 
 	if (i > 98)
 	{
-		while (i > 98)
+		while (i < 98)
 		{
-			printf("%d, ", i--);
+			printf("%d, ", i++);
 			printf("%d\n", i);
 		}
 	}
 	else
 	{
-		while (i < 98)
+		while (i > 98)
 		{
-			printf("%d, ", i++);
+			printf("%d, ", i--);
 			printf("%d\n", i);
 		}
 	}
