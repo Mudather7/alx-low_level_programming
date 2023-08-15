b6e92a902c323188f9631ac0a9f408c02df4ed44
Mudather7 <mudatherabdo00@gmail.com>
2023-08-15 07:07:56 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_alphabet - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+void print_alphabet(void)
+{
+	char n = 'a';
+
+
+	while (n <= 'z')
+	{
+		_putchar(n);
+		n++;
+	}
+
+	_putchar('\n');
+
+	return (0);
+}
