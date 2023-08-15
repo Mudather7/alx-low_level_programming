2bb43475b75c82043ecca722dc038c49a8a76020
Mudather7 <mudatherabdo00@gmail.com>
2023-08-15 12:18:04 -0700

    9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -8,23 +8,26 @@ void times_table(void)
 
 	for (num = 0; num < 10; num++)
 	{
-		_putchar('0');
 
-		for (mult = 1; mult < 10; mult++)
+		for (mult = 0; mult < 10; mult++)
 		{
-			_putchar(',');
-			_putchar(' ');
-
-			prod = num * mult;
-
-			if (prod < 10)
+			prod = mult * num;
+			if (mult == 0)
 			{
+				_putchar(prod + '0');
+			}
+			else if (pord < 10 && mult != 0)
+			{
+				_putchar(',');
 				_putchar(' ');
+				_putchar(' ');
+				_putchar(prod + '0');
 			}
 			else
 			{
+				_putchar(',');
+				_putchar(' ');
 				_putchar((prod / 10) + '0');
-
 				_putchar((prod % 10) + '0');
 			}
 		}
