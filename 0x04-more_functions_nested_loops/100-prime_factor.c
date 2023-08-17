250f6ffc78d55a230e36a010bb5ad4735ee92091
Mudather7 <mudatherabdo00@gmail.com>
2023-08-17 07:09:04 -0700

    101-print_number.c

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <math.h>
+
+/**
+ * main - Entry point.
+ *
+ * Return: 0.
+ */
+int main(void)
+{
+
---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * print_number -  function that prints an integer..
+ *
+ * @n: an integer.
+ *
+ * Return: 0
+ */
+void print_number(int n)
+{
+	int i;
+
+	if (n < 0)
+	{
+		i = -n;
+		_putchar('-');
+	}
+	else
+	{
+		i = n;
+
+		if (i / 10)
+		{
+			_putchar(i / 10);
+		}
+		_putchar((i % 10) + '0');
+	}
+}
