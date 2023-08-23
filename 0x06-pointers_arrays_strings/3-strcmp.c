2a0eaecefa6a9bee36afda463146a968b9c41745
Mudather7 <mudatherabdo00@gmail.com>
2023-08-23 08:52:31 -0700

    3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -10,16 +10,14 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	int i = 0;
-        while (*s1)
-        {
-          if (*s1 != *s2)
-          {
-            i = ((int)*s1 - 48) - ((int)*s2 - 48);
-          }
-          s1++;
-          s2++;
-        }
-        return (i);
+	int i, j;
 
+	i = 0;
+	if (s1[i] == s2[i])
+	{
+		i++;
+	}
+	j = s1[i] - s2[i];
+
+	return (j);
 }
