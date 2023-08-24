b927baaaa87db34c1883a79d1ca2edcdc430e758
Mudather7 <mudatherabdo00@gmail.com>
2023-08-24 03:58:11 -0700

    100-rot13.c

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * *rot13 -  function that encodes a string using rot13.
+ *
+ * @s: a string.
+ *
+ * Return: 0
+ */
+char *rot13(char *s)
+{
+	int i, j;
+	char a[] = {"abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"};
+	char x[] = {"nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM"};
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; a[j] != '\0'; j++)
+		{
+			if (a[j] == s[i])
+			{
+				s[i] == x[j];
+				break;
+			}
+		}
+	}
+	return (s);
+}
