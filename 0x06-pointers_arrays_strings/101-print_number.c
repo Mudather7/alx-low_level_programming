5d048378adfcec538c79671a7ea766b2b1de0245
Mudather7 <mudatherabdo00@gmail.com>
2023-08-24 04:05:25 -0700

    101-print_number.c

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -13,10 +13,6 @@ void print_number(int n)
 		_putchar('-');
 		n = -n;
 	}
-	if (n == 0)
-	{
-		_putchar('0');
-	}
 	if (n / 10)
 	{
 		print_number(n / 10);
@@ -24,4 +20,3 @@ void print_number(int n)
 	_putchar((n % 10) + '0');
 
 }
-
