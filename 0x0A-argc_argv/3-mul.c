3b50cbce26fc357e9a29b19734bf3b6bda841ddd
Mudather7 <mudatherabdo00@gmail.com>
2023-09-01 09:18:31 -0700

    3-mul.c

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry point.
+ *
+ * @argc: count of the argument.
+ *
+ * @argv: an array of pointer to string.
+ */
+int main(int argc, char *argv[])
+{
+	int mult = 0;
+
+	if (argc > 1)
+	{
+		mult = atoi(argv[1]) * atoi(argv[2]);
+		printf("%i\n", mult);
+	}
+	else 
+	{
+		printf("Error\n");
+		return (1);
+	}
+	return (0);
+}	
