940903f5eed6e4dc54338c94640f2d46966281f9
Mudather7 <mudatherabdo00@gmail.com>
2023-09-01 12:35:39 -0700

    4-add.c

---
+++ 0x09-static_libraries/4-add.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+/**
+ * main - Entry point.
+ *
+ * @argc: count of the arguments.
+ *
+ * @argv: an array of pointer to string.
+ *
+ * Return: 0 or 1.
+ */
+int main(int argc, char *argv[])
+{
+	int sum = 0, i;
+
+	if (argc > 1)
+	{
+		for (i = 1; i < argc; i++)
+		{
+			int n;
+			char *str;
+
+			str = argv[i];
+			for (n = 0; str[n] != '\0'; n++)
+			{
+				if (str[n] < 48 || str[n] > 57)
+				{
+					printf("Error\n");
+					return (1);
+				}
+			}
+		}
+	}
+	for (i = 1; i < argc; i++)
+	{
+		sum += atoi(argv[i]);
+	}
+	printf("%d\n", sum);
+	return (0);
+}
