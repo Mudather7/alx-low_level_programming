ca70a11cd7c16e0263c61a6e98c76cfe37849b89
Mudather7 <mudatherabdo00@gmail.com>
2023-09-01 08:02:36 -0700

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Entry point.
+ *
+ * @argc: is a count of the arguments.
+ *
+ * @argv: an array of pointers to the strings.
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
