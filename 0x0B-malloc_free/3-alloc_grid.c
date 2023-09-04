9b2e55950adcd34045f3ba117057ce51575beabe
Mudather7 <mudatherabdo00@gmail.com>
2023-09-04 10:22:48 -0700

    3-alloc_grid.c

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -17,13 +17,23 @@ int **alloc_grid(int width, int height)
 	int i;
 
 	if (width == 0 || height == 0)
+	{
 		return (NULL);
+	}
+
 	a = (int **)malloc(height * sizeof(int *));
+
 	if (a == 0)
+	{
 		return (NULL);
+	}
 	for (i = 0; i < height; i++)
+	{
 		a[i] = malloc(width * sizeof(int));
+	}
 	if (a != NULL)
+	{
 		return (a);
+	}
 	return (NULL);
 }
