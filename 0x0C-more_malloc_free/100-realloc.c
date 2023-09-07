b92d711414f1cc6eea7fe6ccbbc1200974f7f079
Mudather7 <mudatherabdo00@gmail.com>
2023-09-07 13:18:40 -0700

    100-realloc.c

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -29,6 +29,8 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	else if (ptr == NULL)
 	{
 		p = malloc(new_size);
+		free(ptr);
+		return (p);
 	}
 	else if (new_size == 0 && ptr != NULL)
 	{
