bc672de51e34c383e246263dc543e478b156ddd8
Mudather7 <mudatherabdo00@gmail.com>
2023-09-07 07:00:09 -0700

    1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -17,8 +17,10 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	unsigned int i, j, l1, l2;
 	char *ptr;
 
-	if (s1 == NULL || s2 == NULL)
-		return (NULL);
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
 	for (l1 = 0; s1[l1] != '\0'; l1++)
 		;
 	for (l2 = 0; s2[l2] != '\0'; l2++)
