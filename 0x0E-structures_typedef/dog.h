77afd4171e214a0040270983df88b12c472b1b0d
Mudather7 <mudatherabdo00@gmail.com>
2023-09-11 07:58:47 -0700

    dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - a structuer.
+ *
+ * @name: first element of the structuer.
+ *
+ * @age: second element of the structuer.
+ *
+ * @owner: third element of the structuer.
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
