25b9ae182b9ab5ad398086e893433e5194070e90
Mudather7 <mudatherabdo00@gmail.com>
2023-09-13 08:26:11 -0700

    1-array_iterator.c

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -12,10 +12,11 @@
  */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-	size_t i;
+	size_t i = 0;
 
-	for (i = 0; i < size; i++)
+	while (i < size)
 	{
 		action(array[i]);
+		i++;
 	}
 }
