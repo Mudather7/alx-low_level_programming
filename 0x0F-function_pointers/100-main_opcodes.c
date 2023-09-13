b9a245abae587bef573eb099109e93fb5980f2f3
Mudather7 <mudatherabdo00@gmail.com>
2023-09-13 12:54:14 -0700

    100-main_opcodes.c

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - program that prints the opcodes of its own main function.
+ *
+ * @argc: number of arguments.
+ *
+ * @argv: array of arguments.
+ *
+ * Retrun: Always 0
+ */
+int main(int argc, char **argv)
+{
+	char *ptr = (char *)main;
+	int i;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+
+	i = atoi(argv[1]);
+
+	if (i < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+
+	while (i--)
+	{
+		printf("%02hhx%s", *ptr++, i ? " " : "\n");
+	}
+	return (0);
+}
