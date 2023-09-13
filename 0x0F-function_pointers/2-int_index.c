c91a7a0f9902df3fc21afe3c5d01f78b70c67ca0
Mudather7 <mudatherabdo00@gmail.com>
2023-09-13 09:23:22 -0700

    2-int_index.c

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -16,10 +16,14 @@ int int_index(int *array, int size, int (*cmp)(int))
 
 	if (size <= 0)
 		return (-1);
-	for (i = 0; i < size; i++)
+
+	if (array != 0 && cmp != 0)
 	{
-		if (cmp(array[i]) != 0)
+		for (i = 0; i < size; i++)
+		{
+			if (cmp(array[i]) != 0)
 			return (i);
+		}
 	}
 	return (-1);
 }
