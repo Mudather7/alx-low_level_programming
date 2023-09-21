74e84c25352eb40c522f36f4ec355415516be084
Mudather7 <mudatherabdo00@gmail.com>
2023-09-21 10:44:20 -0700

    100-first.c

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+/**
+ * before_main - function that prints a string before main function.
+ */
+void before_main(void)
+{
+	printf("You're beat! and yet, you must allow,\n"
+		"I bore my house upon my back!\n");
+}
