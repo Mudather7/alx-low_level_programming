f79e7a1e8e3fc90916ea64167b39021c4875f8bd
Mudather7 <mudatherabdo00@gmail.com>
2023-09-21 09:42:53 -0700

    1-list_len.c

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,8 +1,9 @@
 #include "lists.h"
 /**
- * list_len - function that returns the number of elements in a linked list_t list.
+ * list_len - function that returns the number of elements
+ * in a linked list_t list.
  *
- * @h: pointer to the first node
+ * @h: pointer to the first node.
  *
  * Return: number of elements.
  */
